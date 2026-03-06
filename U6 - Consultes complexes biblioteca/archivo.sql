886355a6e8203485355cb6d73671aefd7a8c27c9
Fran <frodriguez4@gmail.com>
2026-03-07 00:52:13 +0100

    have advance a little bit the activity

--- U6 - Consultes complexes biblioteca/archivo.sql
+++ U6 - Consultes complexes biblioteca/archivo.sql
@@ -1,41 +1,65 @@
-SELECT L.titol, g.nom FROM LLIBRE L, llibre_genere LG, genere G WHERE l.id=lg.id_llibre AND g.nom=lg.nom_genere; --1
-
-SELECT l.titol, a.nom || ' ' || a.cognoms AS NOMBREAUTOR FROM LLIBRE L,autor_llibre AL,AUTOR A WHERE l.id=al.id_llibre AND a.id=al.id_autor; --2
-
-SELECT l.titol,g.nom, a.nom || ' ' || a.cognoms AS NOMBREAUTOR FROM LLIBRE L, autor_llibre AL, AUTOR A, llibre_genere LG, genere G WHERE l.id=al.id_llibre AND al.id_autor=a.id AND l.id=lg.id_llibre AND lg.nom_genere=g.nom; --3
-
-SELECT l.titol FROM LLIBRE L,autor_llibre AL, autor A WHERE l.id=al.id_llibre AND a.id=al.id_autor GROUP BY l.titol HAVING COUNT(A.ID)=1; --4
-
-SELECT a.nom || ' ' || a.cognoms AS NOMBRE_AUTOR, SUM(l.exemplars) AS TOTAL_EJEMPLARES FROM LLIBRE L, autor_llibre AL, autor A WHERE l.id=al.id_llibre AND a.id=al.id_autor GROUP BY a.nom,a.cognoms; --5
-
 --Sense JOIN
 --1. Llista els llibres (títol) amb el seu gènere (nom).
 --Columnes: títol del llibre, nom del gènere
+SELECT L.titol, g.nom 
+FROM LLIBRE L, llibre_genere LG, genere G 
+WHERE l.id=lg.id_llibre AND g.nom=lg.nom_genere; --1
 
 --2. Llista els títols i autor (nom i llinatge) dels llibres d'autors no espanyols.
 --Columnes: títol llibre, nom i llinatge autor
+SELECT l.titol, a.nom || ' ' || a.cognoms AS NOMBREAUTOR 
+FROM LLIBRE L,autor_llibre AL,AUTOR A 
+WHERE l.id=al.id_llibre AND a.id=al.id_autor; --2
 
 --3. Llista els títols, el gènere (nom) i l'autor (nom i llinatges) de cada llibre. (Si un llibre té més d'un autor o gènere, el seu títol sortirà repetit).
 --Columnes: títol llibre, nom gènere, nom i llinatges autor
+SELECT l.titol,g.nom, a.nom || ' ' || a.cognoms AS NOMBREAUTOR 
+FROM LLIBRE L, autor_llibre AL, AUTOR A, llibre_genere LG, genere G 
+WHERE l.id=al.id_llibre AND al.id_autor=a.id AND l.id=lg.id_llibre AND lg.nom_genere=g.nom; --3
 
 --4. Llista els llibres (títol) amb només un autor.
 --Columnes: títol llibre
+SELECT l.titol 
+FROM LLIBRE L,autor_llibre AL, autor A 
+WHERE l.id=al.id_llibre AND a.id=al.id_autor 
+GROUP BY l.titol HAVING COUNT(A.ID)=1; --4
 
 --5. Llista el nombre d'exemplars totals de cada autor (nom i llinatges).
 --Columnes: nom i llinatges autor, número d'exemplars d'entre tots els seus llibres
+SELECT a.nom || ' ' || a.cognoms AS NOMBRE_AUTOR, SUM(l.exemplars) AS TOTAL_EJEMPLARES 
+FROM LLIBRE L, autor_llibre AL, autor A 
+WHERE l.id=al.id_llibre AND a.id=al.id_autor 
+GROUP BY a.nom,a.cognoms; --5
 
 --Amb JOIN
 --6. Llista els autors (nom i llinatges) sense llibres.
 --Columnes: nom i llinatges autor
+SELECT A.NOM, A.COGNOMS 
+FROM AUTOR A 
+LEFT JOIN AUTOR_LLIBRE AL ON A.ID = AL.ID_AUTOR 
+WHERE AL.ID_LLIBRE IS NULL;
 
 --7. Llista els llibres (títol) amb el seu gènere (nom).
 --Columnes: títol llibre, nom gènere
+SELECT L.TITOL, G.NOM 
+FROM LLIBRE L 
+JOIN LLIBRE_GENERE LG ON L.ID = LG.ID_LLIBRE 
+JOIN GENERE G ON G.NOM = LG.NOM_GENERE;
 
 --8. Llista els gèneres (nom) sense llibres.
 --Columnes: nom gènere
+SELECT G.NOM 
+FROM GENERE G 
+LEFT JOIN LLIBRE_GENERE LG ON G.NOM = LG.NOM_GENERE 
+WHERE LG.ID_LLIBRE IS NULL;
 
 --9. Llista els títols i autor (nom i llinatge) dels llibres d'autors espanyols.
 --Columnes: títol llibre, nom i llinatge autor
+SELECT L.TITOL, A.NOM, A.COGNOMS 
+FROM LLIBRE L 
+JOIN AUTOR_LLIBRE AL ON L.ID = AL.ID_LLIBRE 
+JOIN AUTOR A ON A.ID = AL.ID_AUTOR 
+WHERE UPPER(A.NACIONALITAT) = 'ESP';
 
 --10. Llista els títols, el gènere (nom) i l'autor (nom i llinatges) de cada llibre. (Si un llibre té més d'un autor o gènere, el seu títol sortirà repetit). Mostra només els que tenen autor conegut i gènere.
 --Columnes: títol llibre, nom gènere, nom i llinatges autor
